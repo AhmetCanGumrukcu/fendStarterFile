3228f8946b3559e348b4f3176eb8a727dfba28aa
AhmetCanGumrukcu <parsdizayn@gmail.com>
2018-03-28 11:22:55 +0300

    Add pug
    Gulp file js update and add pug props, sample page added

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var sass = require('gulp-sass');
 var watch = require('gulp-watch');
 var concat = require("gulp-concat");
 var sourcemaps = require("gulp-sourcemaps");
+var pug = require('gulp-pug');
 
 gulp.task('sass', function () {
     return gulp.src('./sass/*.scss')
@@ -22,8 +23,15 @@ gulp.task('scripts', function () {
         .pipe(gulp.dest('./js/'));
 });
 
-gulp.task('sass:watch', function () {
+gulp.task('views', function buildHTML() {
+    return gulp.src('views/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest("./"))
+  });
+
+gulp.task('watch', function () {
     gulp.watch('./sass/*.scss', ['sass']);
+    gulp.watch('./views/*.pug', ['views']);
 });
 
-gulp.task('default', ["sass:watch", "scripts"]);
+gulp.task('default', ["watch", "scripts","views"]);
